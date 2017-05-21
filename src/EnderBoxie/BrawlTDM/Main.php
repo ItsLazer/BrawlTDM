a4d0fe75e63b4f55e4df9397f2bd7a6f88af55fc
Lazer <lazergamez290@gmail.com>
2017-05-22 09:57:41 +1000

    The basics of starting a plugin...

--- src/EnderBoxie/BrawlTDM/Main.php
+++ src/EnderBoxie/BrawlTDM/Main.php
@@ -1,4 +1,15 @@
+<?php
 namespace EnderBoxie\BrawlTDM;
-
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
-
+class main extends PluginBase {
+    public function onLoad(){
+        $this->getLogger()->info("BrawlTDM loading.");
+    }
+    public function onEnable(){
+        $this->getLogger()->info("BrawlTDM enabled.");
+    }
+    public function onDisable(){
+        $this->getLogger()->info("BrawlTDM disabled.");
+    }
+}
